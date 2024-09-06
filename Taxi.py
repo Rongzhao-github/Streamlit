5cf75e7c5b59aaeaf153336af8732883ede614a0
Rongzhao-github <rongzhao@email.com>
2024-09-06 17:34:50 +0800

    Update Taxi.py

--- Taxi.py
+++ Taxi.py
@@ -16,4 +16,4 @@ d = st.number_input("What is the distance (in units)?", min_value=0.0, step=0.1)
 # Calculate and display fare
 if st.button("Calculate Fare"):
     calculated_fare = fare(d)
-    st.success(f"Based on the assumption of booking fee of $3.30, the calculated fare is: ${calculated_fare:.2f}")
+    st.success(f"Based on the assumption of booking fee of $$3.30, the calculated fare is: ${calculated_fare:.2f}")
